e589ab008999fed982bfa0eb8bd5729ad06f9ba1
noblecp <noblecp1@gmail.com>
2023-10-11 14:37:02 +0000

    dockerfiles

---
+++ oath2-config.js
@@ -0,0 +1,11 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: "",
+    clientSecret: "",
+    callbackURL: '' // Replace with your app's callback URL
+}, (accessToken, refreshToken, profile, done) => {
+    // Save user information or perform other necessary actions
+    return done(null, profile);
+}));
